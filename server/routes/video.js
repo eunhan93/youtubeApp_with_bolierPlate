54e0ff2284b585d339167da595eb969f7ece7138
eunhan93 <eunhan0503@gmail.com>
2020-10-13 15:37:10 +0900

    youtube app

--- server/routes/video.js
+++ server/routes/video.js
@@ -3,6 +3,24 @@ const router = express.Router();
 const { Video } = require("../models/Video");
 
 const { auth } = require("../middleware/auth");
+const multer = require('multer');
+
+let storage = multer.diskStorage({
+    destination : (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Data.now()}_${file.originalname}`);
+    },
+    fileFilter : (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        if (ext !== '.mp4'){
+            return cb(res.status(400).end('only mp4 is allowed'), false);
+        }
+        cb(null, true)
+    }
+})
+const upload = multer({storage: storage}).single('file');
 
 //=================================
 //             User
@@ -10,6 +28,12 @@ const { auth } = require("../middleware/auth");
 
 router.post('/uploadfiles', (req, res) => {
     // 서버에 저장
+    upload(req, res, err => {
+        if(err) {
+            return res.json({success : false, err})
+        } 
+        return res.json({success : true, url : res.req.file.path, fileName : res.req.file.filename})
+    })
 })
 
 module.exports = router;
